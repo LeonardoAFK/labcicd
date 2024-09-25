611a671052217188967b64b561a31327113aef55
Leonardo Araque <leonardo.araque@udea.edu.co>
2024-09-25 04:52:52 -0500

    Primer Commit

---
+++ src/main/java/com/udea/labcidcd/LabcidcdApplication.java
@@ -0,0 +1,13 @@
+package com.udea.labcidcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabcidcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabcidcdApplication.class, args);
+	}
+
+}
